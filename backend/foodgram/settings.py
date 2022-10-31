143755d14dbe40778ac8f685e77d51d5d2b04ba2
Андрей Янковский <tiamoon@yandex.ru>
2022-10-31 18:00:29 +0300

    Отправка на первое ревью.

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -77,8 +77,6 @@ DATABASES = {
     }
 }
 
-AUTH_USER_MODEL = 'users.User'
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
