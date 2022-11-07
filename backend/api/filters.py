6c9e5b105c1a6198d51e0813b7026d2725d73f2a
Андрей Янковский <tiamoon@yandex.ru>
2022-11-07 13:28:56 +0300

    Исправление

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,7 +1,6 @@
 from django_filters.rest_framework import FilterSet, NumberFilter, filters
+from recipes import models
 from rest_framework.filters import SearchFilter
-
-from recipes.models import Recipe, Tag
 
 
 class RecipeFilters(FilterSet):
@@ -9,7 +8,7 @@ class RecipeFilters(FilterSet):
     tags = filters.ModelMultipleChoiceFilter(
         field_name='tags__slug',
         to_field_name='slug',
-        queryset=Tag.objects.all()
+        queryset=models.Tag.objects.all()
     )
     is_favorited = NumberFilter(method='filter_is_favorited')
     is_in_shopping_cart = NumberFilter(method='filter_shopping_cart')
@@ -25,7 +24,7 @@ class RecipeFilters(FilterSet):
         return queryset
 
     class Meta:
-        model = Recipe
+        model = models.Recipe
         fields = ('author', 'tags', 'is_favorited', 'is_in_shopping_cart')
 
 
